ff8783ca662a0e1c09e24a371c1ce705cc1f51dc
João Victor Vilela de Mendonça <victorvilela00@gmail.com>
2023-05-25 10:29:08 -0300

    ajuste no arquivo padrão .env

--- frontend/src/api/backendApi.js
+++ frontend/src/api/backendApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import 'dotenv/config';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: process.env.API_URL,
     timeout: 1000,
 });
 
